1b28d0a3ee44dc1b6f5c617e3de1e938e150a135
cheolclass <cheolclass@gmail.com>
2026-09-15 17:29:02 +0900

    Refactor endianness demo in main.cpp
    Updated pointer casting and output formatting for endianness demonstration.

--- main.cpp
+++ main.cpp
@@ -1,1 +1,25 @@
+#include <iostream>
+#include <bitset>
+#include <cstdint>
 
+using namespace std;
+ 
+int main()
+{
+	//int i = 0x12345678;
+	uint32_t	i = 0x12345678;
+
+	//unsigned char* pC = (unsigned char*) &i;
+	unsigned char* pC = reinterpret_cast<unsigned char*> (&i);
+
+	// Big-endian vs. Little-endian
+	//cout << hex << (int)(*pC) << ", " << dec << (int)*pC << ", " << bitset<8>(*pC) << endl;
+	cout << hex << static_cast<int>(*pC) << ", " << 
+		dec << static_cast<int>(*pC) << ", " << bitset<8>(*pC) << endl;
+	cout << bitset<8>(*(pC+1))  << endl;
+	cout << bitset<8>(*(pC + 2)) << endl;
+	cout << hex << static_cast<int>(*(pC + 3)) << ", " << 
+		dec << static_cast<int>(*(pC + 3)) << ", " << bitset<8>(*(pC + 3)) << endl; // 18, 0001 0010
+
+	return 0;
+}
